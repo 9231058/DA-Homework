eb0aee6202859f72227ad470d5142090ce081aae
Parham Alvani <parham.alvani@gmail.com>
2015-02-09 20:00:52 +0330

    Homework-2 problem 2 started.

---
+++ Homework-1/2/main.c
@@ -0,0 +1,20 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 09-02-2015
+ *
+ * [] Last Modified : Mon 09 Feb 2015 08:00:47 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+
+#include "qsort.h"
+
+int main(int argc, char *argv[])
+{
+	
+}
---
+++ Homework-1/2/qsort.h
@@ -0,0 +1,12 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : qsort.h
+ *
+ * [] Creation Date : 09-02-2015
+ *
+ * [] Last Modified : Mon 09 Feb 2015 07:59:16 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
