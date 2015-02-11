8e5dab91915646ec0164bf45e3365271aca9ed6f
Parham Alvani <parham.alvani@gmail.com>
2015-02-11 22:35:56 +0330

    building structure is added.

---
+++ SkyLine/src/building.h
@@ -0,0 +1,22 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : building.h
+ *
+ * [] Creation Date : 11-02-2015
+ *
+ * [] Last Modified : Wed 11 Feb 2015 10:32:00 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#ifndef BUILDING_H
+#define BUILDING_H
+
+struct building {
+	int height;
+	int start_point;
+	int end_point;
+};
+
+#endif
