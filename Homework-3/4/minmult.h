b50985b4ef27eb270ea92e1ff817e5891e77cf8a
Parham Alvani <parham.alvani@gmail.com>
2015-02-24 12:28:29 +0330

    Homework-3 problem 4 is added.

---
+++ Homework-3/4/minmult.h
@@ -0,0 +1,28 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : minmult.h
+ *
+ * [] Creation Date : 24-02-2015
+ *
+ * [] Last Modified : Tue 24 Feb 2015 12:00:35 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#ifndef MINMULT_H
+#define MINMULT_H
+
+#include <stdio.h>
+
+/*
+ * TODO add comment
+*/
+int minmult(int n, const int d[]);
+
+/*
+ *
+*/
+void minmult_fprint(FILE *stream);
+
+#endif
