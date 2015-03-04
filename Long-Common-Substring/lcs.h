23064a7f321d3f8d2b66fcfde0bb1aeddbb61a1e
Parham Alvani <parham.alvani@gmail.com>
2015-03-04 05:24:55 +0330

    Long Common Substring project is added.

---
+++ Long-Common-Substring/lcs.h
@@ -0,0 +1,20 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : lcs.h
+ *
+ * [] Creation Date : 04-03-2015
+ *
+ * [] Last Modified : Wed 04 Mar 2015 05:20:49 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#ifndef LCS_H
+#define LCS_H
+
+int lcs(const char *A, const char *B);
+
+void lcs_fprint(FILE *stream);
+
+#endif
---
+++ Long-Common-Substring/main.c
@@ -0,0 +1,27 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 04-03-2015
+ *
+ * [] Last Modified : Wed 04 Mar 2015 05:20:36 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+#include <string.h>
+
+#include "lcs.h"
+
+int main(int argc, char *argv[])
+{
+	char A[1024];
+	char B[1024];
+
+	scanf("%s %s", A, B);
+
+	printf("%d\n", lcs(A, B));
+	lcs_fprint(stdout);
+}
