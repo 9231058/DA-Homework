8637907d75dfb4edfccb34ad2d8095db6a47471a
Parham Alvani <parham.alvani@gmail.com>
2015-04-12 07:49:34 +0430

    N-Queen project is added.

---
+++ N-Queen/main.c
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 12-04-2015
+ *
+ * [] Last Modified : Sun 12 Apr 2015 07:27:54 AM IRDT
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include "n-queen.h"
+
+int main(int argc, char *argv[])
+{
+	n_queen(4);
+}
---
+++ N-Queen/n-queen.h
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : n-queen.h
+ *
+ * [] Creation Date : 12-04-2015
+ *
+ * [] Last Modified : Sun 12 Apr 2015 07:24:55 AM IRDT
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#ifndef N_QUEEN_H
+#define N_QUEEN_H
+
+void n_queen(int size);
+
+#endif
