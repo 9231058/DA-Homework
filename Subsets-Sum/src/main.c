cadcddc28631197b035d3f61b9f1d67d7e794c76
Parham Alvani <parham.alvani@gmail.com>
2015-05-08 18:00:16 +0430

    subsets-sum project is added.

---
+++ Subsets-Sum/src/main.c
@@ -0,0 +1,21 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 08-05-2015
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2015 Parham Alvani.
+*/
+#include "subsets-sum.h"
+
+int main(int argc, char *argv[])
+{
+	int w[] = {1, 2, 3, 4, 5, 6, 7};
+	int W = 6;
+	subsets_sum(7, w, W);
+}
---
+++ Subsets-Sum/src/subsets-sum.h
@@ -0,0 +1,19 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : subsets-sum.h
+ *
+ * [] Creation Date : 08-05-2015
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2015 Parham Alvani.
+*/
+#ifndef SUBSETS_SUM_H
+#define SUBSETS_SUM_H
+
+void subsets_sum(int size, const int *weight, int w_max);
+
+#endif
